b42a2cc3642368d945543b222384c6a8aaf1c087
rupin <rupin.chheda@gmail.com>
2018-03-11 13:18:26 +0530

    Auto Code Generation

--- formPay.js
+++ formPay.js
@@ -1,32 +1,77 @@
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<script> 
 function completePayment()
 {
  var options = {
     "key": "rzp_test_WGzU46Mxz4bbRd",
     "amount": "1250000", // 2000 paise = INR 20
-    "name": "Merchant Name",
-    "description": "Purchase Description",
-	
-	
+    "name": "CuriosityGym",
+    "description": "Registration for Innovation Hub",	
     
     "handler": function (response){
-        alert(response.razorpay_payment_id);
-    },
-    "prefill": {
-        "name": "Harshil Mathur",
-        "email": "harshil@razorpay.com"
-    },
-    "notes": {
-        "studentName": "Hello World",
-		"studentName1": "Hello World1",
-		"studentName2": "Hello World2",
+        //alert();
+		console.log(jQuery(".content").children().not(".success_payment_message").hide());
+		jQuery("#transaction_id").html(response.razorpay_payment_id)
+		jQuery(".success_payment_message").show();
 		
     },
+    
     "theme": {
-        "color": "#F37254"
-    }
+        "color": "#08c"
+    },
+	"notes":{}
 };
-  var rzp1 = new Razorpay(options);
- rzp1.open();
- e.preventDefault();
+	fieldIDs=["field_cat_fname", "field_cat_lname","field_cat_std", "field_cat_div"];
+	
+	if(validateFields(fieldIDs))
+	{
+		jQuery(".error_message_payment").hide();
+		updatedOptions=constructNotes(fieldIDs, options)
+		console.log(updatedOptions);
+		//constructNotes(fieldIDs)
+		var rzp1 = new Razorpay(updatedOptions);
+		rzp1.open();
+	}
+	else
+	{
+		jQuery(".error_message_payment").show();
+	}
+  //var rzp1 = new Razorpay(options);
+  //rzp1.open();
+  
+ 
 
 }
+
+function validateFields(fields)
+{
+	for(i=0;i<fields.length;i++)
+	{
+			fieldValue=jQuery("#"+fields[i]).val()+"";
+			if(fieldValue=="")
+			{
+				return false;
+			}
+			
+	}
+	return true;
+}
+
+function constructNotes(fields, options)
+{
+	
+	for(i=0;i<fields.length;i++)
+	{
+			
+			fieldName=fields[i];
+			fieldValue=jQuery("#"+fieldName).val()+"";	
+			options.notes[fieldName]=fieldValue;   		
+			//notesobj.notes[fieldName]=fieldValue  
+			
+			
+	}
+	return options;
+}
+
+
+</script>
